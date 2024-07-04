3265faac39525e8ba8ff36cf95566fd03999e1ba
User Konstantin <Konstantin19111996.Agaltsov@yandex.ru>
2024-07-04 17:43:56 +0300

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        File myFile = new File("C:\\Program Files\\Test\\document.txt");
+        System.out.println("File name: " + myFile.getName());
+        System.out.println("Parent folder: " + myFile.getParent());
+        if(myFile.exists()){
+            System.out.println("File exists");
+        } else{
+            System.out.println("File not found");
+        }
+
+        System.out.println("File size: " + myFile.length());
+        if(myFile.canRead()){
+            System.out.println("File can be read");
+        } else {
+            System.out.println("File can not be read");
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Время записи " + (endTime - startTime));
+
+        OutputStreamWriter streamWriter = new OutputStreamWriter(new FileOutputStream("C:\\Program Files\\Test\\document.txt"));
+        streamWriter.write(12312);
+        streamWriter.close();
+    }
 }
--- src/Main1.java
+++ src/Main1.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main1 {
+import java.io.File;
+
+public class Main1 {
+    public static void main(String[] args) {
+        File dir = new File("C:\\Program Files\\Test");
+        if(dir.isDirectory()){
+
+            for(File item : dir.listFiles()){
+                if(item.isDirectory()){
+                    System.out.println(item.getName() + " \t folder");
+                } else {
+                    System.out.println(item.getName() + " \t file");
+                }
+            }
+
+        }
+    }
+
 }
